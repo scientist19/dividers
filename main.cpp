3a7e1da805e2ff1a0ba19f4728014b705e0778aa
scientist19 <40214489+scientist19@users.noreply.github.com>
2018-09-19 17:47:13 +0300

    Fixed

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin >> n;
+	
+	vector <int> list;
+	for (int i = 1; i <= sqrt(n); i++){
+		
+		if (n % i == 0){
+			
+			list.push_back(i);
+			if (n/i != i) list.push_back(n/i);
+		}
+	}
+	
+	for (int i = 0; i < list.size(); i++)
+		cout << list[i] << " ";
+}
